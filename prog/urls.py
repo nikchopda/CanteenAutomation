b41ba7498108c2aae58fed76889c76786cb42659
kavisha14 <solankikavisha24@gmail.com>
2020-11-10 11:20:48 +0530

    Added New Chef functionality - urls.py

--- prog/urls.py
+++ prog/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^adminloginvalidation', views.validation, name='validation'),
+	url(r'^addchef', views.addchef, name='addchef'),
     url(r'^forget',views.forget,name='forget'),
     url(r'^fp',views.fp,name='fp'),
     url(r'^logout',views.logout,name='logout'),
